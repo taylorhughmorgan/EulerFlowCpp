e759fd2823b4ff7fa189cf18a50362c2e78c67f2
Hugh Morgan <serebrum@gmail.com>
2025-02-16 08:27:24 -0700

    added Jameson-Shmidt-Turkel third-order differencing schemes

--- EulerFlowCpp/BoundaryConditions.cpp
+++ EulerFlowCpp/BoundaryConditions.cpp
@@ -1,20 +1,20 @@
 #include "BoundaryConditions.hpp"
 
 
-void reflective(pde_state& u, const pde_state& grid, ident ids, double value) {
+static void reflective(pde_state& u, const pde_state& grid, ident ids, double value) {
 	u[ids[0]] = -u[ids[1]];
 }
 
-void gradient(pde_state& u, const pde_state& grid, ident ids, double value) {
+static void gradient(pde_state& u, const pde_state& grid, ident ids, double value) {
 	double dx = grid[ids[1]] - grid[ids[0]];
 	u[ids[0]] = u[ids[1]] + value * dx;
 }
 
-void extrapolated(pde_state& u, const pde_state& grid, ident ids, double value) {
+static void extrapolated(pde_state& u, const pde_state& grid, ident ids, double value) {
 	u[ids[0]] = 2 * u[ids[1]] - u[ids[2]];
 }
 
-void constant_value(pde_state& u, const pde_state& grid, ident ids, double value) {
+static void constant_value(pde_state& u, const pde_state& grid, ident ids, double value) {
 	// constant value boundary condition
 	u[ids[0]] = value;
 }
@@ -24,28 +24,23 @@ BCFunction agnosticBCs(const ident& ids, validBCs bc_id, double bc_value)
 	// develop boundary conditions agnostic to whether it is applied to the upper or lower bound
 	std::function<void(pde_state& u, const pde_state& grid)> bc;
 
-	if (bc_id == validBCs::REFLECTIVE)
-	{
+	if (bc_id == validBCs::REFLECTIVE) {
 		// applying reflective boundary condition
 		bc = std::bind(reflective, std::placeholders::_1, std::placeholders::_2, ids, bc_value);
 	}
-	else if (bc_id == validBCs::GRADIENT) 
-	{
+	else if (bc_id == validBCs::GRADIENT) {
 		// gradient boundary condition
 		bc = std::bind(gradient, std::placeholders::_1, std::placeholders::_2, ids, bc_value);
 	}
-	else if (bc_id == validBCs::EXTRAPOLATED)
-	{
+	else if (bc_id == validBCs::EXTRAPOLATED) {
 		// extrapolated boundary condition
 		bc = std::bind(extrapolated, std::placeholders::_1, std::placeholders::_2, ids, bc_value);
 	}
-	else if (bc_id == validBCs::CONSTANT)
-	{
+	else if (bc_id == validBCs::CONSTANT) {
 		// constant value boundary condition
 		bc = std::bind(constant_value, std::placeholders::_1, std::placeholders::_2, ids, bc_value);
 	}
-	else
-	{
+	else {
 		throw std::invalid_argument("Boundary Condition has not been implemented. Valid options are: gradient:<value>, extrapolated, constant:<value>.");
 	}
 	return bc;
